b18fa33ef721c31d1884b0036a9e83e809a53a7b
Zack <zack.duck@outlook.com>
2022-04-02 22:08:04 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static String UserNumeral = "";
+    public static String[] UserSArray = new String[15];
+    public static Integer[] UserIArray = new Integer[15];
+
+    public static void main(String[] args) {
+        AddToUserS();
+        AddToUserI();
+        System.out.println("Your numeral converted into a number is " + MakeNum());
+    }
+
+    public static void AddToUserS() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your number in roman numerals up to M");
+        UserNumeral = input.nextLine();
+
+            for (int i = 0; i < UserNumeral.length(); i++) {
+                UserSArray = UserNumeral.split("");
+            }
+    }
+
+    public static void AddToUserI() {
+
+        for (int i = 0; i < UserNumeral.length(); i++) {
+            if (UserSArray[i].equalsIgnoreCase("i")) {
+                UserIArray[i] = 1;
+            } else if (UserSArray[i].equalsIgnoreCase("v")) {
+                UserIArray[i] = 5;
+            } else if (UserSArray[i].equalsIgnoreCase("x")) {
+                UserIArray[i] = 10;
+            } else if (UserSArray[i].equalsIgnoreCase("l")) {
+                UserIArray[i] = 50;
+            } else if (UserSArray[i].equalsIgnoreCase("c")) {
+                UserIArray[i] = 100;
+            } else if (UserSArray[i].equalsIgnoreCase("d")) {
+                UserIArray[i] = 500;
+            } else if (UserSArray[i].equalsIgnoreCase("m")) {
+                UserIArray[i] = 1000;
+            } else System.out.println("All of these letters are not roman numerals");
+        }
+    }
+
+    public static int MakeNum(){
+        int Num = 0;
+        UserIArray[UserNumeral.length()] = 0;
+        for (int i = 0; i < UserNumeral.length(); i++) {
+            if (UserIArray[i] < UserIArray[i + 1] ){
+                int temp = UserIArray[i + 1] - UserIArray[i];
+                Num = Num + temp;
+                i = i + 1;
+            }else {
+                Num = Num + UserIArray[i];
+            }
+        }
+        return Num;
+    }
+}
